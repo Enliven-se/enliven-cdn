7bc0b4e516bd9745608d88630159f4a3465ded86
Daniel « Klokie » Grossfeld <github@klokie.com>
2019-05-13 12:09:40 +0200

    removed Blaze templating dependency; customized name of repo

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'nitrolabs:cdn',
-  version: '1.3.0',
+  name: 'enliven:cdn',
+  version: '1.3.1',
   summary: 'Serve Meteor content from a CDN',
-  git: 'https://github.com/nitrolabs/meteor-cdn',
+  git: 'https://github.com/Enliven-se/enliven-cdn',
   documentation: 'README.md'
 });
 
@@ -11,16 +11,14 @@ Package.onUse(function(api) {
   api.export('CDN','server');
   api.export('CDN','client');
   api.use('webapp','server');
-  api.use('templating','client');
   api.use('browser-policy', {weak: true});
-  api.addFiles('lib/template.js','client');
   api.addFiles('lib/client.js','client');
   api.addFiles('lib/server.js','server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('nitrolabs:cdn');
+  api.use('enliven:cdn');
   api.addFiles('tests/client.js','client');
   api.addFiles('tests/server.js','server');
 
